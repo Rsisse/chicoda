0d12a0eba0723a4f59ac57442f918add744e873b
Rsisse <econstant002@ensc.fr>
2018-10-21 13:21:48 +0200

    fonciton de lecture fichier xml

---
+++ ProjetIA/TestXML/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TestXML
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            List<Question> maListe = Question.CreateListQuestions();
+
+            foreach (var item in maListe)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+
+    }
+}
